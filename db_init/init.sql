1b39b4631b46c4b6b2283186082f125b33692da4
Reza Mollagholipour <reza76py@gmail.com>
2025-05-27 21:12:42 +1000

    v1_all worked with Docker & with Local too.

--- db_init/init.sql
+++ db_init/init.sql
@@ -7,11 +7,22 @@ CREATE TABLE IF NOT EXISTS nodes (
     z FLOAT NOT NULL
 );
 
+-- CREATE TABLE IF NOT EXISTS elements (
+--     id INT AUTO_INCREMENT PRIMARY KEY,  -- Crucial for unique elements
+--     start_node INT NOT NULL,
+--     end_node INT NOT NULL,
+--     FOREIGN KEY (start_node) REFERENCES nodes(id),
+--     FOREIGN KEY (end_node) REFERENCES nodes(id),
+--     UNIQUE (start_node, end_node)  -- Prevent duplicate elements
+-- );
+
+
+
 CREATE TABLE IF NOT EXISTS elements (
-    id INT AUTO_INCREMENT PRIMARY KEY,  -- Crucial for unique elements
-    start_node INT NOT NULL,
-    end_node INT NOT NULL,
-    FOREIGN KEY (start_node) REFERENCES nodes(id),
-    FOREIGN KEY (end_node) REFERENCES nodes(id),
-    UNIQUE (start_node, end_node)  -- Prevent duplicate elements
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    start_node_id INT NOT NULL,
+    end_node_id INT NOT NULL,
+    FOREIGN KEY (start_node_id) REFERENCES nodes(id),
+    FOREIGN KEY (end_node_id) REFERENCES nodes(id),
+    UNIQUE (start_node_id, end_node_id)
 );
